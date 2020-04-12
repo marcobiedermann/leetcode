ee3e80fc9112b4f156a1bd7129fe69a827d4faee
Marco Biedermann <marco.biedermann@gmx.de>
2020-04-12 10:25:40 +0200

    feat: 'Two Sum'
    Add 'Two sum' problem

---
+++ problems/1-two-sum/index.test.ts
@@ -0,0 +1,9 @@
+import twoSum from '.';
+
+describe('twoSum', () => {
+  it('should return indices of the two numbers such that they add up to a specific target', () => {
+    expect.assertions(1);
+
+    expect(twoSum([2, 7, 11, 15], 9)).toStrictEqual([0, 1]);
+  });
+});
