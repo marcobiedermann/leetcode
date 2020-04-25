b8d01c9f01fc8965325367e46a15d5c3e2cec808
Marco Biedermann <marco.biedermann@gmx.de>
2020-04-25 19:01:05 +0200

    feat: 'Jump Game'
    Add 'Jump Game' alternative

--- problems/55-jump-game/index.ts
+++ problems/55-jump-game/index.ts
@@ -12,4 +12,20 @@ function canJump(nums: number[]): boolean {
   return lastPos === 0;
 }
 
+// function canJump(nums: number[]): boolean {
+//   const { length } = nums;
+
+//   let reachable = 0;
+
+//   for (let i = 0; i < length; i += 1) {
+//     if (reachable < i) {
+//       return false;
+//     }
+
+//     reachable = Math.max(reachable, i + nums[i]);
+//   }
+
+//   return true;
+// }
+
 export default canJump;
