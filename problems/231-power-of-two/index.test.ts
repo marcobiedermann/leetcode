f69af3af9c45510ac1f562189e63836a2d5a9e44
Marco Biedermann <marco.biedermann@gmx.de>
2020-06-08 21:03:10 +0200

    feat: 'Power of Two'
    Add 'Power of Two' problem

---
+++ problems/231-power-of-two/index.test.ts
@@ -0,0 +1,11 @@
+import isPowerOfTwo from '.';
+
+describe('isPowerOfTwo', () => {
+  it('should check if number is a power of two', () => {
+    expect.assertions(3);
+
+    expect(isPowerOfTwo(1)).toStrictEqual(true);
+    expect(isPowerOfTwo(16)).toStrictEqual(true);
+    expect(isPowerOfTwo(218)).toStrictEqual(false);
+  });
+});
---
+++ problems/231-power-of-two/index.ts
@@ -0,0 +1,5 @@
+function isPowerOfTwo(n: number): boolean {
+  return n > 0 && !(n & (n - 1));
+}
+
+export default isPowerOfTwo;
