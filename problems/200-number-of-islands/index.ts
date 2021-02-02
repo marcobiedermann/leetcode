08a6176df9d6c3692c562e37b46c7eeaf66961a5
Marco Biedermann <marco.biedermann@gmx.de>
2021-02-02 10:46:07 +0100

    refactor: 'Number of Islands'
    Refactor 'Number of Islands' problem

--- problems/200-number-of-islands/index.ts
+++ problems/200-number-of-islands/index.ts
@@ -1,24 +1,33 @@
-function dfs(grid: string[][], i: number, j: number): void {
-  if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] === '0') {
+/* eslint-disable no-param-reassign */
+
+function bfs(grid: string[][], row: number, column: number): void {
+  if (
+    row < 0 ||
+    row >= grid.length ||
+    column < 0 ||
+    column >= grid[row].length ||
+    grid[row][column] === '0'
+  ) {
     return;
   }
 
-  grid[i][j] = '0';
+  grid[row][column] = '0';
 
-  dfs(grid, i + 1, j);
-  dfs(grid, i - 1, j);
-  dfs(grid, i, j + 1);
-  dfs(grid, i, j - 1);
+  bfs(grid, row + 1, column);
+  bfs(grid, row - 1, column);
+  bfs(grid, row, column + 1);
+  bfs(grid, row, column - 1);
 }
 
 function numIslands(grid: string[][]): number {
   let count = 0;
 
-  for (let i = 0; i < grid.length; i += 1) {
-    for (let j = 0; j < grid[i].length; j += 1) {
-      if (grid[i][j] === '1') {
+  for (let row = 0; row < grid.length; row += 1) {
+    for (let column = 0; column < grid[row].length; column += 1) {
+      if (grid[row][column] === '1') {
         count += 1;
-        dfs(grid, i, j);
+
+        bfs(grid, row, column);
       }
     }
   }
